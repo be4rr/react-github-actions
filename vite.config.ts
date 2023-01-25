31039bcdefe762575f800406a67636f9f94a3251
be4rr <adgjmptw.montblanc@gmail.com>
2023-01-25 14:12:25 +0900

    Revert "環境変数の読み方変更"
    This reverts commit e337817b81fda9bef5e38a9659c0ffb47dd9adb0.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.GITHUB_PAGES ? "react-github-actions" : "./",
+  base: process.env.GITHUB_PAGES ? "react-github-actions" : "./",
   plugins: [react()],
 })
